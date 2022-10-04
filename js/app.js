7c934d5d15cfeb62ef2408ff81ac8fc26556cc45
MatthewChong <chiongmatthew96@gmail.com>
2022-10-04 17:07:26 +0800

    update .js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ const hamburger = document.getElementById("hamburger");
 const navBar = document.getElementById("header-nav-container");
 const overlay = document.getElementById("overlay");
 const intro = document.getElementById("intro");
-const navLinks = document.getElementsByClassName("header-nav-links");
+const navLinks = document.querySelectorAll(".header-nav-links");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
