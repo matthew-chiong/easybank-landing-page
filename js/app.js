dce32209b3c4a1205dcabba8790c28cd359995d7
MatthewChong <chiongmatthew96@gmail.com>
2022-10-04 17:01:44 +0800

    dasfw

--- js/app.js
+++ js/app.js
@@ -2,10 +2,20 @@ const hamburger = document.getElementById("hamburger");
 const navBar = document.getElementById("header-nav-container");
 const overlay = document.getElementById("overlay");
 const intro = document.getElementById("intro");
+const navLinks = document.getElementsByClassName("header-nav-links");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navBar.classList.toggle("active");
   overlay.classList.toggle("active");
   intro.classList.toggle("active");
+});
+
+navLinks.forEach((links) => {
+  links.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navBar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    intro.classList.toggle("active");
+  });
 });
